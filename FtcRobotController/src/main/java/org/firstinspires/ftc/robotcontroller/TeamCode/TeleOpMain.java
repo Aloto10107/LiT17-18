a9daadec4fd2d5f4f4ba96aae479fc9fb058b23d
A League of Their Own <aloto10107@gmail.com>
2017-11-11 11:12:16 -0500

    Update TeleOpMain.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TeamCode/TeleOpMain.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TeamCode/TeleOpMain.java
@@ -4,7 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class TeleOpMain extends OpMode{
-
+    
+        //Abdullah is GAY
+    
     private MotorController frontLeftMotor;
     private MotorController backLeftMotor;
     private MotorController frontRightMotor;
